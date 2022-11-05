57dd64752f90e489a6cb0b35dda46da723d5a93e
lanhoangpham <lanhoangpham.ph@gmail.com>
2022-11-05 10:16:04 +0700

    C4 - modify RunKeyCipher

--- RunKeyCipher/main.cpp
+++ RunKeyCipher/main.cpp
@@ -1,16 +1,25 @@
 #include "RunKeyCipher.h"
 
 int main() {
+    //xâu từ khóa
 	char key[] = "cipher";
-
-	char message[] = "Cryptography";
-
-	int *int_key = arrayAsciiOfString(key, sizeof(key));
-
-	char *c = runKeyCipher(message, key);
-	
-	cout << stringLength(key);
-	for (int i = 0; i < stringLength(c); i++) {
-		cout << c[i];
+    //xâu message
+	char message[] = "XinChaoQuynhHuong";
+	//in ra màn hình chuỗi trước khi mã hóa
+	cout << "Chuoi truoc ma hoa: " ;
+	for ( int i = 0; i <stringLength(message); i++) cout << message[i];
+    //xâu lưu trữ ký tự sau mã hóa
+	char *c = cryptographyRunKeyCipher(message, key);
+ 	//in ra màn hình kết quả message sau mã hóa theo hệ mật run key cipher
+ 	cout << "Chuoi sau ma hoa: " ;
+	for (int i = 0; i < stringLength(message) + 1; i++)  cout << c[i];
+	cout << endl;
+	//xâu lưu trữ ký tự sau giải mã hóa theo hệ mật run key cipher
+	char *d = decryptographyRunKeyCipher(c, key);
+	//in ra màn hình kết quả
+	cout << "Chuoi sau giai ma: ";
+	for ( int i = 0; i < stringLength(message); i++){
+	    cout << d[i];
 	}
+	return 0;
 }
